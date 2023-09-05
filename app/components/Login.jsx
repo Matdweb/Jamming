97782d1c3e8125ae76dc810804e51f233fe8e806
mat_dweb <mat.dweb28@gmail.com>
2023-09-05 16:52:52 -0600

    create sign in functionlity in login page

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,3 +1,5 @@
+'use client'
+import { signIn } from "next-auth/react";
 
 function Login() {
     return (
@@ -5,7 +7,7 @@ function Login() {
             <div className='mt-16 sm:mt-36 px-4 sm:px-6 py-8 shadow-black max-w-[37rem] rounded-[2.5rem] flex justify-center items-center flex-col flex-nowrap bg-black/70'>
                 <h3 className='text-center title-bold mb-6 sm:mb-8'>Login to create new awesome custome playlists</h3>
                 <p className='mb-24 sm:mb-16'>Click now to Log In with your Spotify account!</p>
-                <button className='btn-spotify' />
+                <button className='btn-spotify' onClick={() => signIn()} />
             </div>
         </section>
     )
