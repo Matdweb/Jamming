868d41bde80c4832fb07013980c40c40789fe2fa
mat_dweb <mat.dweb28@gmail.com>
2023-09-05 17:35:15 -0600

    secure home page with authentication

--- app/page.js
+++ app/page.js
@@ -1,9 +1,17 @@
+'use client'
+import { signOut, useSession } from 'next-auth/react'
 import Login from './components/Login'
 
 export default function Home() {
-  return (
-    <>
-      <Login />
-    </>
-  )
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <div>
+        <h1 className='title-bold'>Welcome {session?.token?.name}</h1>
+        <button onClick={() => signOut()}>Sign Out</button>
+      </div>
+    )
+  } else {
+    return <Login />
+  }
 }
