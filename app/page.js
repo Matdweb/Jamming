8148a235b0101088d6a579c5b306460ac33adc7d
mat_dweb <mat.dweb28@gmail.com>
2023-09-05 19:00:52 -0600

    test new Button

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 'use client'
 import { signOut, useSession } from 'next-auth/react'
 import Login from './components/Login'
+import Button from './components/Button';
 
 export default function Home() {
   const { data: session } = useSession();
@@ -8,7 +9,7 @@ export default function Home() {
     return (
       <div>
         <h1 className='title-bold'>Welcome {session?.token?.name}</h1>
-        <button onClick={() => signOut()}>Sign Out</button>
+        <Button error={false} toggle={signOut}>Sign Out</Button>
       </div>
     )
   } else {
