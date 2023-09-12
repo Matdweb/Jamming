f57d911a198c992d100b53fc3d43cd0a579395c4
Matias Vargas <mat.dweb28[1>
2023-09-12 17:43:36 -0600

    fix sizing on 'Login with Spotify' box

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -4,7 +4,7 @@ import { signIn } from "next-auth/react";
 function Login() {
     return (
         <section className='w-full flex justify-center items-center px-9 pb-20'>
-            <div className='mt-16 sm:mt-36 px-4 sm:px-6 py-8 shadow-black max-w-[37rem] rounded-[2.5rem] flex justify-center items-center flex-col flex-nowrap bg-black/70'>
+            <div className='max-w-[37rem] mt-16 sm:mt-36 px-4 sm:px-6 py-8 shadow-black bg-black/70 rounded-[2.5rem] flex justify-center items-center flex-col flex-nowrap'>
                 <h3 className='text-center title-bold mb-6 sm:mb-8'>Login to create new awesome custome playlists</h3>
                 <p className='mb-24 sm:mb-16'>Click now to Log In with your Spotify account!</p>
                 <button className='btn-spotify' onClick={() => signIn()} />
