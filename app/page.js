e6db274f56fe032d8c5f246240ba850f25cb5518
Matias Vargas <mat.dweb28[1>
2023-09-12 18:15:08 -0600

    integrate new containers for cool songs in main page

--- app/page.js
+++ app/page.js
@@ -2,14 +2,18 @@
 import { signOut, useSession } from 'next-auth/react'
 import Login from './components/Login'
 import SongSearcher from './components/SongSearcher';
+import SongsContainer from './components/SongsContainer';
+import PlaylistContainer from './components/PlaylistContainer';
 
 export default function Home() {
   const { data: session } = useSession();
   if (session) {
     return (
       <div>
-        <section className='w-full h-full flex justify-center items-start flex-row flex-wrap mt-24'>
+        <section className='w-full h-full flex justify-center items-start flex-row flex-wrap md:mt-24 mt-16 pb-32'>
           <SongSearcher />
+          <SongsContainer />
+          <PlaylistContainer />
         </section>
       </div>
     )
