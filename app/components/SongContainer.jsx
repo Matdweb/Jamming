fa5c31685f4a6599d8c58996a0db4bccb93a576e
Matias Vargas <mat.dweb28[1>
2023-09-21 21:24:30 -0600

    implement the add & remove funcionalities in layout

--- app/components/SongContainer.jsx
+++ app/components/SongContainer.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+'use client'
+import { useContext, useEffect } from 'react'
 import Image from 'next/image'
 import addIcon from '../../public/asstes/img/add.png'
 import removeIcon from '../../public/asstes/img/minus.png'
+import { SpotifyContext } from '@/context/SpotifyContextProvider'
 
-function SongContainer({ songName, artist, album, isRemovable = false }) {
+function SongContainer({ content, isRemovable = false }) {
+  const { playlist: { addToPlaylist, removeFromPlaylist } } = useContext(SpotifyContext);
+
   return (
     <div className='h-20 sm:h-24 border-b flex justify-between items-center flex-row'>
       <div className='pl-3 pr-9 flex justify-center items-start flex-col text-start'>
-        <h4 className='song-title-bold'>{songName}</h4>
-        <p className='ligth-text'>{artist}  |  {album}</p>
+        <h4 className='song-title-bold'>{content.name}</h4>
+        <p className='ligth-text'>{content.artist}  |  {content.album}</p>
       </div>
       <span className='pr-4 cursor-pointer'>
-        <Image src={ isRemovable ? removeIcon : addIcon } alt='add icon' />
+        <Image
+          className='cursor-pointer active:scale-95'
+          src={isRemovable ? removeIcon : addIcon}
+          alt='add icon'
+          onClick={() => {
+            if(!isRemovable){
+              addToPlaylist(content);
+            } else {
+              removeFromPlaylist(content.id);
+            }
+          }}
+        />
       </span>
     </div>
   )
