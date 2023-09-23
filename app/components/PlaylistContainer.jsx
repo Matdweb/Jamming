73614b5465353fd09f7869ca1cff7ea0a198ea4b
Matias Vargas <mat.dweb28[1>
2023-09-22 23:09:58 -0600

    integrate Button animations when making a request

--- app/components/PlaylistContainer.jsx
+++ app/components/PlaylistContainer.jsx
@@ -9,22 +9,53 @@ import createNewPlaylist from "@/lib/Spotify/createNewPlaylist/createNewPlaylist
 
 function PlaylistContainer() {
     const [playlistName, setPlaylistName] = useState('New Playlist');
-    const { playlist: { playlistSongs } } = useContext(SpotifyContext)
-
+    const { token: { getAccessToken }, playlist: { playlistSongs } } = useContext(SpotifyContext)
     const { data: session } = useSession();
     const refresh_token = session?.token.accessToken;
-    const { token: { getAccessToken } } = useContext(SpotifyContext);
+
+    const [buttonState, setButtonState] = useState('loading');
+    const [buttonText, setButtonText] = useState('Save this in Spotify')
 
     const handleChange = ({ target }) => {
         setPlaylistName(target.value);
     }
 
-    const createPlaylist = async () => {
-        const access_token = await getAccessToken(refresh_token);
-        const response = await createNewPlaylist(access_token, playlistName, playlistSongs);
-
-        console.log(response);
+    const handleSuccessCall = () => {
+        setButtonState('successful');
+        setButtonText('Ready 🥳');
+        resetButtonText();
     }
+
+    const handleErrorCall = () => {
+        setButtonState('error');
+        setButtonText('Try again in a second!');
+        resetButtonText();
+    }
+
+    const resetButtonText = () => {
+        setTimeout(()=> setButtonText('Save this in Spotify') , 4000);
+    }
+
+    const createPlaylist = async () => {
+        setButtonState('loading');
+        try {
+            const access_token = await getAccessToken(refresh_token);
+            const response = await createNewPlaylist(access_token, playlistSongs);
+            console.log(response);
+            handleSuccessCall();
+        } catch (e) {
+            handleErrorCall();
+            console.log(e);
+        }
+    }
+
+    useEffect(() => {
+        if (playlistSongs.length > 0) {
+            setButtonState('successful');
+        } else {
+            setButtonState('loading');
+        }
+    }, [playlistSongs])
 
     return (
         <div className='max-w-[44rem] flex justify-center items-center sm:px-8 pb-9'>
@@ -49,7 +80,7 @@ function PlaylistContainer() {
                     })}
                 </div>
                 <div className='w-full pt-6 flex justify-center sm:justify-end items-center ml-0 sm:ml-6'>
-                    <Button state={'loading'} toggle={async () => await createPlaylist()}>Save this in Spotify</Button>
+                    <Button state={buttonState} toggle={() => createPlaylist()}>{buttonText}</Button>
                 </div>
             </div>
         </div>
