f21753eda612ceb57eef1c88b004f62895aa3f22
Matias Vargas <mat.dweb28[1>
2023-09-28 21:17:21 -0600

    reorganize layout display

--- app/page.js
+++ app/page.js
@@ -4,20 +4,24 @@ import Login from './components/Login'
 import SongSearcher from './components/SongSearcher';
 import SongsContainer from './components/SongsContainer';
 import PlaylistContainer from './components/PlaylistContainer';
+import Background from './components/Background';
 
 export default function Home() {
   const { data: session } = useSession();
-  if (session) {
-    return (
+  return (
+    <Background>
       <div>
-        <section className='w-full h-full flex justify-center items-start flex-row flex-wrap md:mt-24 mt-16 pb-32'>
-          <SongSearcher />
-          <SongsContainer />
-          <PlaylistContainer />
+        <section className='text-center pt-16 sm:pt-24'>
+          <h2 className='header-black'>Create your custom playlist with</h2>
+          <h1 className='black-title'>Jamming</h1>
         </section>
+        {!session ? <Login /> :
+          <section className='w-full h-full flex justify-center items-start flex-row flex-wrap md:mt-24 mt-16 pb-32'>
+            <SongSearcher />
+            <SongsContainer />
+            <PlaylistContainer />
+          </section>}
       </div>
-    )
-  } else {
-    return <Login />
-  }
+    </Background>
+  )
 }
